a1024b1830368007497f7c57b0da86f4582c5397
Jin <erlking@qq.com>
2015-07-19 23:47:23 +0800

    finish the ranking page

--- config.rb
+++ config.rb
@@ -1,5 +1,6 @@
 require 'compass/import-once/activate'
 require 'compass-normalize'
+encoding = "utf-8"
 # Require any additional compass plugins here.
 # Set this to the root of your project when deployed:
 http_path = "/"
@@ -10,7 +11,7 @@ javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-
+output_style = :compact
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
 
